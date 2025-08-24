50789dcd346e8b64640f40be6112cd126e4cceca
Regeron13 <yuto150310@gmail.com>
2025-08-25 03:12:21 +0900

    ✨ debug modeの追加

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from argparse import ArgumentParser
 from typing import Iterator
 
 import discord
@@ -13,10 +14,15 @@ class Ciel(commands.Bot):
         kwargs.setdefault("command_prefix", "")
         kwargs.setdefault("intents", discord.Intents.default())
         kwargs.setdefault("help_command", None)
+
+        self.debug = kwargs.pop("debug", False)
         super().__init__(*args, **kwargs)
 
     def run(self, *args, **kwargs):
-        kwargs["token"] = os.getenv("DISCORD_TOKEN")
+        token = os.getenv("DISCORD_TOKEN")
+        if self.debug:
+            token = os.getenv("DEBUG_DISCORD_TOKEN", token)
+        kwargs.setdefault("token", token)
         super().run(*args, **kwargs)
 
     def extension_files(self) -> Iterator[str]:
@@ -31,15 +37,32 @@ class Ciel(commands.Bot):
         for name in self.extension_files():
             await self.load_extension(name)
 
+    async def command_sync(self):
+        debug_guild_id = os.getenv("DEBUG_GUILD_ID")
+        if self.debug and debug_guild_id:
+            debug_guild = discord.Object(id=int(debug_guild_id))
+            self.tree.copy_global_to(guild=debug_guild)
+            await self.tree.sync(guild=debug_guild)
+        else:
+            await self.tree.sync()
+
     async def setup_hook(self):
         await self.load_all_extensions()
+        await self.command_sync()
 
     async def on_ready(self):
         print("Ciel Start-up")
 
 
 if __name__ == "__main__":
+    parser = ArgumentParser(description="Generic Discord Bot built with discord.py.")
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode.")
+    args = parser.parse_args()
+
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     dotenv.load_dotenv()
-    bot = Ciel()
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+    bot = Ciel(intents=intents, debug=args.debug)
     bot.run()
