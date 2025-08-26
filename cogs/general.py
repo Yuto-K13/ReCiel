a557cf40b4fba2610c536381734342067bb29899
Regeron13 <yuto150310@gmail.com>
2025-08-27 02:26:40 +0900

    ✨ help コマンド追加

--- cogs/general.py
+++ cogs/general.py
@@ -1,5 +1,8 @@
+from typing import Union
+
 import discord
 from discord import Color, Embed, Interaction, app_commands
+from discord.app_commands import AppCommand, AppCommandGroup, Argument, Command, Group
 from discord.ext import commands
 
 from main import Ciel
@@ -16,6 +19,57 @@ class General(commands.Cog):
         embed = Embed(title="Pong!", color=Color.blue())
         embed.add_field(name="Latency", value=f"{latency:.2f} ms", inline=False)
         await interaction.response.send_message(embed=embed)
+
+    @app_commands.command()
+    async def help(self, interaction: Interaction):
+        """コマンドの詳細説明"""
+
+        def descriptions(
+            command: Union[Command, Group],
+            app_command: Union[AppCommand, AppCommandGroup],
+        ) -> list[str]:
+            descs: list[str] = list()
+            if isinstance(command, Command):
+                descs.append(app_command.mention)
+                descs.append(command.description)
+                if command.parameters:
+                    descs.append("**Parameters**")
+                for param in command.parameters:
+                    descs.append(f"・{param.name}: {param.description}")
+                return descs
+
+            sub_command_map = {
+                sub_command: sub_app_command
+                for sub_command in command.commands
+                for sub_app_command in app_command.options
+                if sub_command.name == sub_app_command.name
+            }
+            for sub_command in command.commands:
+                sub_app_command = sub_command_map.get(sub_command)
+                if sub_app_command is None or isinstance(sub_app_command, Argument):
+                    continue
+                descs.extend(descriptions(sub_command, sub_app_command))
+            return descs
+
+        global_commands = await self.bot.tree.fetch_commands()
+        guild_commands = await self.bot.tree.fetch_commands(guild=interaction.guild)
+        app_command_map = {cmd.name: cmd for cmd in global_commands + guild_commands}
+
+        embed = Embed(title="Help for Ciel", color=Color.blue())
+        for cog_name in self.bot.cogs:
+            cog = self.bot.get_cog(cog_name)
+            if cog is None:
+                continue
+
+            descs = list()
+            for command in cog.get_app_commands():
+                app_command = app_command_map.get(command.name)
+                if app_command is None:
+                    continue
+                descs.extend(descriptions(command, app_command))
+            if descs:
+                embed.add_field(name=cog_name, value="\n".join(descs))
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
 async def setup(bot: Ciel):
